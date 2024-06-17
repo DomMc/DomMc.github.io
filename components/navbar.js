d4c814a31a2a93433e260c2d71c40c4a39f9b642
Dominic McCollum <98114338+DomMc@users.noreply.github.com>
2024-06-17 19:56:11 +0100

    Update navbar.js
    Converted to switch statement based on page ID

--- components/navbar.js
+++ components/navbar.js
@@ -17,28 +17,25 @@ class Navbar extends HTMLElement
         var portfolioLink = `<li><a href="portfolio.html" title="Portfolio"><strong>Portfolio</strong></a></li>`;
         var contactlink = `<li><a href="contact.html" title="Contact"><strong>Contact</strong></a></li>`;
 
-        /* Changes the link styling to highlight the active page to the user, the home page requires additional changes */
-        if (document.body.id == 0)
+        switch (document.body.id)
         {
-            homeLink = `<li><a href="index.html" title="Home" class="active"><strong>Home</strong></a></li>`;
-            aboutLink = `<li><a href="pages/about.html" title="About"><strong>About</strong></a></li>`;
-            portfolioLink = `<li><a href="pages/portfolio.html" title="Portfolio"><strong>Portfolio</strong></a></li>`;
-            contactlink = `<li><a href="pages/contact.html" title="Contact"><strong>Contact</strong></a></li>`;
-        }
-
-        if (document.body.id == 1)
-        {
-            aboutLink = `<li><a href="about.html" title="About" class="active"><strong>About</strong></a></li>`;
-        }
-
-        if (document.body.id == 2)
-        {
-            portfolioLink = `<li><a href="portfolio.html" title="Portfolio" class="active"><strong>Portfolio</strong></a></li>`;
-        }
-
-        if (document.body.id == 3)
-        {
-            contactlink = `<li><a href="contact.html" title="Contact" class="active"><strong>Contact</strong></a></li>`;
+            case '0': // Home page
+                homeLink = `<li><a href="index.html" title="Home" class="active"><strong>Home</strong></a></li>`;
+                aboutLink = `<li><a href="pages/about.html" title="About"><strong>About</strong></a></li>`;
+                portfolioLink = `<li><a href="pages/portfolio.html" title="Portfolio"><strong>Portfolio</strong></a></li>`;
+                contactlink = `<li><a href="pages/contact.html" title="Contact"><strong>Contact</strong></a></li>`;
+                break;
+            case '1': // About page
+                aboutLink = `<li><a href="about.html" title="About" class="active"><strong>About</strong></a></li>`;
+                break;
+            case '2': // Portfolio page
+                portfolioLink = `<li><a href="portfolio.html" title="Portfolio" class="active"><strong>Portfolio</strong></a></li>`;
+                break;
+            case '3': // Contact page
+                contactlink = `<li><a href="contact.html" title="Contact" class="active"><strong>Contact</strong></a></li>`;
+                break;
+            default:
+                break;
         }
 
         /*
