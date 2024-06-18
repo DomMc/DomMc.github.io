0a14345a93d63dff65c3a51dbcf68777aea01038
Dominic McCollum <98114338+DomMc@users.noreply.github.com>
2024-06-18 11:54:38 +0100

    Refactored the navbar
    The navbar will now contain a burger menu for small screens so that options are clearer on mobile.

--- components/navbar.js
+++ components/navbar.js
@@ -9,39 +9,81 @@ class Navbar extends HTMLElement
         super();
     }
 
+    static ShowVerticalMenu()
+    {
+        var pageElement = document.getElementById("navbar");
+
+        /* Only run if the nav bar isn't already in responsive mode. */
+        if (!pageElement.className.includes("responsive"))
+        {
+            pageElement.className += " responsive";
+        }
+    }
+
+    static HideVerticalMenu() 
+    {
+        var pageElement = document.getElementById("navbar");
+
+        /* Only run if the nav bar is in responsive mode. */
+        if (pageElement.className.includes("responsive"))
+        {
+            pageElement.className = "nav";
+        }
+    }
+
     connectedCallback()
     {
         /* The basic link syntax */
-        var homeLink = `<li><a href="../index.html" title="Home"><strong>Home</strong></a></li>`;
-        var aboutLink = `<li><a href="about.html" title="About"><strong>About</strong></a></li>`;
-        var portfolioLink = `<li><a href="portfolio.html" title="Portfolio"><strong>Portfolio</strong></a></li>`;
-        var contactlink = `<li><a href="contact.html" title="Contact"><strong>Contact</strong></a></li>`;
+        var homeLink = `<a href="../index.html" title="Home"><strong>Home</strong></a>`;
+        var aboutLink = `<a href="about.html" title="About"><strong>About</strong></a>`;
+        var portfolioLink = `<a href="portfolio.html" title="Portfolio"><strong>Portfolio</strong></a>`;
+        var contactlink = `<a href="contact.html" title="Contact"><strong>Contact</strong></a>`;
+        var expandLink = '<a href="javascript:void(0)" class="icon" onclick="Navbar.ShowVerticalMenu()">&#9776</a>'
 
         switch (document.body.id)
         {
             case '0': // Home page
-                homeLink = `<li><a href="index.html" title="Home" class="active"><strong>Home</strong></a></li>`;
-                aboutLink = `<li><a href="pages/about.html" title="About"><strong>About</strong></a></li>`;
-                portfolioLink = `<li><a href="pages/portfolio.html" title="Portfolio"><strong>Portfolio</strong></a></li>`;
-                contactlink = `<li><a href="pages/contact.html" title="Contact"><strong>Contact</strong></a></li>`;
+                homeLink = `<a href="index.html" title="Home" class="active"><strong>Home</strong></a>`;
+                aboutLink = `<a href="pages/about.html" title="About"><strong>About</strong></a>`;
+                portfolioLink = `<a href="pages/portfolio.html" title="Portfolio"><strong>Portfolio</strong></a>`;
+                contactlink = `<a href="pages/contact.html" title="Contact"><strong>Contact</strong></a>`;
                 break;
             case '1': // About page
-                aboutLink = `<li><a href="about.html" title="About" class="active"><strong>About</strong></a></li>`;
+                aboutLink = `<a href="about.html" title="About" class="active"><strong>About</strong></a>`;
                 break;
             case '2': // Portfolio page
-                portfolioLink = `<li><a href="portfolio.html" title="Portfolio" class="active"><strong>Portfolio</strong></a></li>`;
+                portfolioLink = `<a href="portfolio.html" title="Portfolio" class="active"><strong>Portfolio</strong></a>`;
                 break;
             case '3': // Contact page
-                contactlink = `<li><a href="contact.html" title="Contact" class="active"><strong>Contact</strong></a></li>`;
-                break;
+                contactlink = `<a href="contact.html" title="Contact" class="active"><strong>Contact</strong></a>`;
+
+                /*
+                    Constructs the navigation bar differently for the last page, so the active page is always shown
+                    in the main navigation bar.
+                */
+                this.innerHTML = '<div class="nav" id="navbar">'
+                    + homeLink
+                    + aboutLink
+                    + contactlink
+                    + portfolioLink
+                    + expandLink
+                    + '</div>';
+
+                return;
             default:
                 break;
         }
 
         /*
-            Constructs the navigation bar.
+            Constructs the navigation bar with the standard link order.
         */
-        this.innerHTML = "<ul>" + homeLink + aboutLink + portfolioLink + contactlink + "</ul>";
+        this.innerHTML  = '<div class="nav" id="navbar">'
+                        + homeLink
+                        + aboutLink
+                        + portfolioLink
+                        + contactlink
+                        + expandLink
+                        + '</div>';
     }
 }
 
